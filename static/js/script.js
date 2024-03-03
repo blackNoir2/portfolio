c00af3bdd1ec8eefb79f7418465f1dc12a0e2ccf
Blacknoir2 <blackNoir2>
2024-03-03 17:46:07 +0000

    create the first part to the call to action

---
+++ static/js/script.js
@@ -0,0 +1,29 @@
+import { TypeAnimator } from "./animatedText.js";
+
+
+
+function displayAnimatedText() {
+    const phrases = ["I am a web developer",
+                     "I am problem solver", 
+                     "I am a proficient in HTML",
+                     "I am proficient in JS",
+                     "I am proficient in CSS",
+                     "I proficent in Python",
+                     "I know Node",
+                     "I am constantly learning and improving myself",
+                     "I am creative"
+                    ];
+
+  
+    const typeText = new TypeAnimator(); 
+    typeText.setPhraseArray(phrases)
+    typeText.setTextToTypeCssSelector("#animated-text");
+    typeText.start();
+
+
+}
+
+
+displayAnimatedText();
+
+
