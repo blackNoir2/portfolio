c1b2e94589743cbbaadb51b1908bd3a2ccdca5ab
blackNoir2 <109542044+blackNoir2@users.noreply.github.com>
2024-03-05 15:08:53 +0000

    Update script.js
    Added JS to handle the mouseover Icons

--- static/js/script.js
+++ static/js/script.js
@@ -23,6 +23,54 @@ function displayAnimatedText() {
 }
 
 
+function toggleHomeIcon() {
+    toggleTextHelper(".home-icon");
+}
+
+
+function toggleAboutMeIcon() {
+    toggleTextHelper(".about-me-icon");
+}
+
+
+function toggleSkillsIcon() {
+    toggleTextHelper(".skills-icon");
+}
+
+
+function toggleProjectIcon() {
+    toggleTextHelper(".projects-icon");
+}
+
+
+function toggleContactUsIcon() {
+    toggleTextHelper(".contact-me-icon");
+}
+
+function toggleTextHelper(iconDivElement, textMenuItemElement = ".menu-item") {
+    const divElements      = document.querySelectorAll(iconDivElement);
+    const menuItemElements = document.querySelectorAll(`${iconDivElement} ${textMenuItemElement}`);
+
+    divElements.forEach((divElement, index) => {
+        divElement.addEventListener("mouseover", () => {
+            divElement.classList.add("display-item");
+            menuItemElements[index].classList.remove("hide-item");
+        });
+
+        divElement.addEventListener("mouseout", () => {
+            divElement.classList.remove("display-item");
+            menuItemElements[index].classList.add("hide-item");
+        });
+    });
+}
+
+
+
 displayAnimatedText();
+toggleHomeIcon();
+toggleAboutMeIcon();
+toggleSkillsIcon();
+toggleProjectIcon();
+toggleContactUsIcon();
 
 
