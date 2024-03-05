628587817ddfd337edbc512cb87190ba2e3968bf
Blacknoir2 <blackNoir2>
2024-03-05 14:24:14 +0000

    Portfolio
    1. Changed the background
    2. Added icons
    3. Added JS function to handle the toggle of the icons
    4. Refactored the css

---
+++ tests/utilsTests/animatedTest.js
@@ -0,0 +1,20 @@
+
+import { describe } from 'node:test';
+import assert   from 'assert';
+
+// Example usage
+assert.strictEqual(1 + 1, 2, '1 + 1 should equal 2');
+assert.ok(true, 'This assertion will pass');
+
+
+import { TypeAnimator } from "../../static/js/utils/animatedText";
+import { assert } from 'node:console';
+
+/**
+ * This file contains unit tests for the TypeAnimator object class aimed at validating its functionality.
+ * The primary objective of these tests is to ensure that the TypeAnimator class functions correctly as a whole.
+ * This is achieved by meticulously testing each individual method within the class while considering its
+ * functionality within the context of object-oriented programming principles.
+ */
+
+
